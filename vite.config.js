6766e278a39c2096c9b6063bd44ab86f044dc7dc
Irina Park <park_irina@naver.com>
2023-02-02 16:51:09 +0900

    deploy test 4

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-web/",
+  base: "portfolio-web",
   root: "./",
   build: { outDir: "dist" },
   // publicDir: "/",
